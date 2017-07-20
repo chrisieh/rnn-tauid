145a1e6a9757a8001706c701e353e7a2f85888b1
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2017-07-20 16:34:25 +0200

    Probability plots

--- scripts/plots/multiclass_probability.py
+++ scripts/plots/multiclass_probability.py
@@ -5,12 +5,10 @@ import h5py
 
 import matplotlib as mpl
 mpl.use("PDF")
-
 from rnn_tauid.common.mpl_setup import mpl_setup
-mpl_setup()
+mpl_setup(scale=0.48)
 
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
 
 
 def main(args):
@@ -23,39 +21,42 @@ def main(args):
         pred = f["score"][idx:]
 
     labels = ["1p0n", "1p1n", "1pXn", "3p0n", "3pXn"]
+    texlabels = [r"$h^\pm$", r"$h^\pm \pi^0$", r"$h^\pm \geq 2 \pi^0$",
+                 r"$3 h^\pm$", r"$3 h^\pm \geq 1 \pi^0$"]
     proba = {}
     for i, label in enumerate(labels):
         sel = (truth == i)
         proba[label] = pred[sel]
 
-
-    fig = plt.figure(figsize=(8, 8))
-    gs = GridSpec(3, 2)
-
     # Probability loop
-    for i, label1 in enumerate(labels):
-        ax = plt.subplot(gs[i // 2, i % 2])
-        ax.set_xlabel("{} probability".format(label1), ha="right", x=1.0)
+    for i, (label1, texlabel1) in enumerate(zip(labels, texlabels)):
+        fig, ax = plt.subplots()
+        ax.set_xlabel("{} mode probability".format(texlabel1), ha="right", x=1)
+        ax.set_ylabel("Normalised number of events", ha="right", y=1)
 
         # Hist constituent loop
         hists = {}
-        for j, label2 in enumerate(labels):
+        colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"]
+        for j, (label2, texlabel2, c) in enumerate(zip(labels, texlabels,
+                                                       colors)):
+            if args.skip_3p and "3p" in label2:
+                continue
+
             ax.hist(proba[label2][:, i], bins=40, range=(0, 1), normed=True,
-                                    histtype="step", label=label2)
-            ax.set_ylim(0, 10)
+                    histtype="step", color=c, label=texlabel2)
+            ax.set_ylim(0, 8)
+            ax.legend(loc="upper center", title="True decay mode:")
 
-        if i == 1:
-            ax.legend()
-
-    gs.tight_layout(fig, pad=0.4)
-    fig.savefig(args.o)
+        fig.savefig("_".join([args.o, label1]) + ".pdf")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("data")
     parser.add_argument("deco")
-    parser.add_argument("-o", default="proba.pdf")
+    parser.add_argument("-o", default="proba")
+
+    parser.add_argument("--skip-3p", action="store_true")
 
     args = parser.parse_args()
     main(args)
